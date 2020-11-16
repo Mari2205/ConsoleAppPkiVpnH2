e23e6a5d516d5f67e2fa738ac6f14d1e21b033c8
Uncharted <uncha@LAPTOP-1PJR3MM8>
2020-11-16 19:53:56 +0100

    Add project files.

---
+++ ConsoleAppPkiVpnH2/ConvertingTool.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPkiVpnH2
+{
+    class ConvertingTool
+    {
+        //public Queue<int> text = new Queue<int>();
+        //public Queue<int> Key = new Queue<int>();
+        //public int GetNumberLenght()
+        //{
+        //    int numL = numbers.Count();
+        //    return numL;
+        //}
+
+        public void ConvertTextToNumbs(char[] collection, Queue<int> queueName)
+        {
+            foreach (char item in collection)
+            {
+                switch (item)
+                {
+                    case 'a':
+                        queueName.Enqueue(0);
+                        break;
+                    case 'b':
+                        queueName.Enqueue(1);
+                        break;
+                    case 'c':
+                        queueName.Enqueue(2);
+                        break;
+                    case 'd':
+                        queueName.Enqueue(3);
+                        break;
+                    case 'e':
+                        queueName.Enqueue(4);
+                        break;
+                    case 'f':
+                        queueName.Enqueue(5);
+                        break;
+                    case 'g':
+                        queueName.Enqueue(6);
+                        break;
+                    case 'h':
+                        queueName.Enqueue(7);
+                        break;
+                    case 'i':
+                        queueName.Enqueue(8);
+                        break;
+                    case 'j':
+                        queueName.Enqueue(9);
+                        break;
+                    case 'l':
+                        queueName.Enqueue(10);
+                        break;
+                    case 'm':
+                        queueName.Enqueue(11);
+                        break;
+                    case 'n':
+                        queueName.Enqueue(12);
+                        break;
+                    case 'o':
+                        queueName.Enqueue(13);
+                        break;
+                    case 'p':
+                        queueName.Enqueue(14);
+                        break;
+                    case 'q':
+                        queueName.Enqueue(15);
+                        break;
+                    case 'r':
+                        queueName.Enqueue(16);
+                        break;
+                    case 's':
+                        queueName.Enqueue(17);
+                        break;
+                    case 't':
+                        queueName.Enqueue(18);
+                        break;
+                    case 'u':
+                        queueName.Enqueue(19);
+                        break;
+                    case 'v':
+                        queueName.Enqueue(20);
+                        break;
+                    case 'w':
+                        queueName.Enqueue(21);
+                        break;
+                    case 'x':
+                        queueName.Enqueue(22);
+                        break;
+                    case 'y':
+                        queueName.Enqueue(23);
+                        break;
+                    case 'z':
+                        queueName.Enqueue(24);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleAppPkiVpnH2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPkiVpnH2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VigenCipherEncryption encryption = new VigenCipherEncryption();
+            ConvertingTool convert = new ConvertingTool();
+
+
+            Console.Write("enter you´re text: ");
+            string ursText = Console.ReadLine();
+            Console.Write("enter you´re Key: ");
+            string ursKey = Console.ReadLine();
+
+
+            char[] charArrayText = ursText.ToCharArray();
+            char[] charArrayKey = ursKey.ToCharArray();
+
+            Queue<int> text = new Queue<int>();
+            Queue<int> key = new Queue<int>();
+
+            convert.ConvertTextToNumbs(charArrayKey, key);
+            convert.ConvertTextToNumbs(charArrayText, text);
+
+
+            foreach (int itemRow in key)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("test");
+                foreach (var itemColumn in text)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("prit =" + itemRow + " || " + itemColumn);
+                    Console.WriteLine("this is from the big table) " + encryption.VCTable()[itemRow, itemColumn]);
+                }
+            }
+            Console.ResetColor();
+
+            Console.ReadKey();
+        }
+    }
+}
